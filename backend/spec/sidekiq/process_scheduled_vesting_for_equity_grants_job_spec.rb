e408b93fa2312fa430f91c3937e50e96beccd9b4
Smit <smitp.contact@gmail.com>
2025-08-05 01:21:55 +0530

    misc: test corrections

--- backend/spec/sidekiq/process_scheduled_vesting_for_equity_grants_job_spec.rb
+++ backend/spec/sidekiq/process_scheduled_vesting_for_equity_grants_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ProcessScheduledVestingForEquityGrantsJob do
   describe "#perform" do
     let(:vesting_schedule) { create(:vesting_schedule) }
     let!(:equity_grant_vests_as_per_schedule1) { create(:equity_grant, :vests_as_per_schedule, vesting_schedule:) }
-    let!(:equity_grant_vests_as_per_schedule2) { create(:equity_grant, :vests_as_per_schedule, vesting_schedule:, period_ended_at: 1.day.ago) }
+    let!(:equity_grant_vests_as_per_schedule2) { create(:equity_grant, :vests_as_per_schedule, vesting_schedule:, board_approval_date: 2.days.ago, period_ended_at: 1.day.ago) }
     let!(:equity_grant_vests_as_per_schedule3) { create(:equity_grant, :vests_as_per_schedule, vesting_schedule:, period_ended_at: 5.hours.from_now) }
     let!(:equity_grant_vests_as_per_schedule4) { create(:equity_grant, :vests_as_per_schedule, vesting_schedule:, period_ended_at: 1.day.from_now) }
     let!(:equity_grant_vests_as_per_schedule5) { create(:equity_grant, :vests_as_per_schedule, vesting_schedule:, period_ended_at: 1.month.from_now, accepted_at: nil) }

f67fd80346a44f8ae68a8485629a6415f1b4fa8d
Smit <smitp.contact@gmail.com>
2025-08-16 17:43:39 +0530

    3 workers & cached

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 4,
+  workers: 3,
   reporter: process.env.CI ? [["list"], ["html"]] : "list",
   use: {
     baseURL: "https://test.flexile.dev:3101",
